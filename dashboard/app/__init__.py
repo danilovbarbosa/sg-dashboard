878d3fc51fa806245424c2da9199ee86793c950e
Maira Carvalho <m.brandao.carvalho@tue.nl>
2015-11-09 17:08:04 +0100

    WIP: improving dashbaord

--- dashboard/app/__init__.py
+++ dashboard/app/__init__.py
@@ -6,8 +6,10 @@ and sets up logging.
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
