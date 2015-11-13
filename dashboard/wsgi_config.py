3fa18e5b3354fcd2fc81b8e60c3da1ef5d299e5b
Maira B. Carvalho <m.brandao.carvalho@tue.nl>
2015-11-13 19:25:14 +0100

    Minor changes in deployment files

--- dashboard/wsgi_config.py
+++ dashboard/wsgi_config.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path = '/home/carvalhomb/sg-dashboard/dashboard_app '
+path = '/home/carvalhomb/sg-dashboard/dashboard'
 if path not in sys.path:
     sys.path.append(path)
 
